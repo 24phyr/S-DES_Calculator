5c43dee58409cc833c24a5bddb873483f0a27f05
Fardin <fardinislam41@gmail.com>
2025-09-26 23:41:33 +0600

    Removed Redundant Functions

--- script.js
+++ script.js
@@ -1,83 +1,71 @@
 // P10(3527410986)
-function permuteP10(keyArray) {
-    return [keyArray[2], keyArray[4], keyArray[1], keyArray[6], keyArray[3], keyArray[9], keyArray[0], keyArray[8], keyArray[7], keyArray[5]];
+function permuteP10(keyBits) {
+    return [keyBits[2], keyBits[4], keyBits[1], keyBits[6], keyBits[3], keyBits[9], keyBits[0], keyBits[8], keyBits[7], keyBits[5]];
 }
 
-function splitP10(P10Array) {
-    return [P10Array.slice(0,5), P10Array.slice(5)];
+function splitTenBits(tenBits) {
+    return [tenBits.slice(0,5), tenBits.slice(5)];
 }
 
-function leftLS1(left) {
-    return [left[1],left[2],left[3],left[4],left[0]];
+function circularLS1(bits) {
+    return [bits[1],bits[2],bits[3],bits[4],bits[0]];
 }
 
-function rightLS1(right) {
-    return [right[1],right[2],right[3],right[4],right[0]];
+function combineHalves(left,right) {
+    return left.concat(right);
 }
 
-function combineLS1(left,right) {
-    return [left[1],left[2],left[3],left[4],left[0],right[1],right[2],right[3],right[4],right[0]];
-}
-
-function leftLS2(leftLS1Shifted) {
-    return [leftLS1Shifted[2],leftLS1Shifted[3],leftLS1Shifted[4],leftLS1Shifted[0],leftLS1Shifted[1]];
-}
-
-function rightLS2(rightLS1Shifted) {
-    return [rightLS1Shifted[2],rightLS1Shifted[3],rightLS1Shifted[4],rightLS1Shifted[0],rightLS1Shifted[1]];
-}
-
-function combineLS2(leftLS1Shifted,rightLS1Shifted) {
-    return [leftLS1Shifted[2],leftLS1Shifted[3],leftLS1Shifted[4],leftLS1Shifted[0],leftLS1Shifted[1],rightLS1Shifted[2],rightLS1Shifted[3],rightLS1Shifted[4],rightLS1Shifted[0],rightLS1Shifted[1]];
+function circularLS2(bits) {
+    return [bits[2],bits[3],bits[4],bits[0],bits[1]];
 }
 
 // P8(637485109)
-function permuteP8(shifted) {
-    return [shifted[5],shifted[2],shifted[6],shifted[3],shifted[7],shifted[4],shifted[9],shifted[8]];
+function permuteP8(tenBits) {
+    return [tenBits[5],tenBits[2],tenBits[6],tenBits[3],tenBits[7],tenBits[4],tenBits[9],tenBits[8]];
 }
 
 function SDES() {
     let plaintext = document.getElementById("plaintext").value;
     let key = document.getElementById("key").value;
 
-    let ptArray = plaintext.split("").map(Number);
-    let keyArray = key.split("").map(Number);
+    let plaintextBits = plaintext.split("").map(Number);
+    let keyBits = key.split("").map(Number);
 
-    console.log("Plaintext: ", ptArray);
-    console.log("Key: ", keyArray);
+    console.log("Plaintext: ", plaintextBits);
+    console.log("Key: ", keyBits);
 
-    let P10Array = permuteP10(keyArray);
-    console.log("P10 Permutation: ", P10Array);
+    let permutedP10 = permuteP10(keyBits);
+    console.log("P10 Permutation: ", permutedP10);
 
-    let [left, right] = splitP10(P10Array);
+    let [left, right] = splitTenBits(permutedP10);
     console.log(left);
     console.log(right);
 
-    let leftLS1Shifted = leftLS1(left);
-    let rightLS1Shifted = rightLS1(right);
+    let leftLS1Shifted = circularLS1(left);
+    let rightLS1Shifted = circularLS1(right);
 
-    let shiftedLS1 = combineLS1(left, right);
+    let shiftedLS1 = combineHalves(leftLS1Shifted, rightLS1Shifted);
     console.log(shiftedLS1);
 
-    let P8Array = permuteP8(shiftedLS1);
-    console.log("P8 Permutation", P8Array);
+    let K1 = permuteP8(shiftedLS1);
+    console.log("K1: ", K1);
 
-    let leftLS2Shifted = leftLS2(leftLS1Shifted);
-    let rightLS2Shifted = rightLS2(rightLS1Shifted);
-    let shiftedLS2 = combineLS2(leftLS1Shifted, rightLS1Shifted);
+    let leftLS2Shifted = circularLS2(leftLS1Shifted);
+    let rightLS2Shifted = circularLS2(rightLS1Shifted);
+    let shiftedLS2 = combineHalves(leftLS2Shifted, rightLS2Shifted);
     let K2 = permuteP8(shiftedLS2);
     console.log("K2: ", K2);
 
     document.getElementById("output").innerHTML = 
-    "<p>Plaintext: " + ptArray.join("") + "</p>" +
-    "<p>Key: " + keyArray.join("") + "</p>" +
-    "<p>P10 Permutation: " + P10Array.join("") + "</p>" +
+    "<p>Plaintext: " + plaintextBits.join("") + "</p>" +
+    "<p>Key: " + keyBits.join("") + "</p>" +
+    "<p>P10 Permutation: " + permutedP10.join("") + "</p>" +
     "<p>left: " + left.join("") + "</p>" +
     "<p>right: " + right.join("") + "</p>" +
     "<p>LS-1 on Left: " + leftLS1Shifted.join("") + "</p>" +
     "<p>LS-1 on Right: " + rightLS1Shifted.join("") + "</p>" +
     "<p>Combining: " + shiftedLS1.join("") + "</p>" +
-    "<p>K1: " + P8Array.join("") + "</p>" +
+    "<p>K1: " + K1.join("") + "</p>" +
     "<p>LS-2 on Left: " + leftLS2Shifted.join("") + "</p>" +
     "<p>LS-2 on Right: " + rightLS2Shifted.join("") + "</p>" +
     "<p>Combining: " + shiftedLS2.join("") + "</p>" +
